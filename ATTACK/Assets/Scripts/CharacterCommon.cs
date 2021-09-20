762baa9c24de4a7f336dc34df02c5d41d070a238
Oliver Eriksson <olieri@kth.se>
2021-09-20 10:06:28 +0200

    Fix automatic fight simulation and make scene look more alive (alter lighting and game field depth) for demo

--- ATTACK/Assets/Scripts/CharacterCommon.cs
+++ ATTACK/Assets/Scripts/CharacterCommon.cs
@@ -1,24 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class CharacterCommon : MonoBehaviour
 {
     private GameManager gameManager;
-    
     private Team team;
+    private Attack attack;
 
-    public GameObject attack;
+    public bool CanAttack => attack.CanAttack;
 
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        Attack();
-    }
-
-    void Update()
-    {
-        
+        attack = GetComponentInChildren<Attack>();
+        Assert.IsNotNull(attack);
     }
 
     public void SetTeam(Team team)
@@ -31,11 +29,17 @@ public class CharacterCommon : MonoBehaviour
         return this.team;
     }
 
-    private void Attack()
+    /**
+     * Performs an attack and starts character's Attack animation
+     * @returns true if an attack simulation was started, false otherwise
+     */
+    public bool AttackRandom()
     {
         Transform targetPoint = gameManager.GetRandomTarget(team);
         Vector3 DirToTarget = (targetPoint.position - gameObject.transform.position).normalized;
 
         transform.rotation = Quaternion.LookRotation(DirToTarget);
+
+        return attack.StartSimulation();
     }
 }

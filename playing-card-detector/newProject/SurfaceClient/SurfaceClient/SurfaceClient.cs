3fde26ebdd8166b358382a70418b552d6c01f3c4
Anders Steen <anders.steen98@gmail.com>
2021-09-23 09:54:25 +0200

    set up SurfaceClient program

---
+++ playing-card-detector/newProject/SurfaceClient/SurfaceClient/SurfaceClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace SurfaceClient {
+	class SurfaceClient {
+
+		private static UdpClient udpClient = new UdpClient();
+
+		static void Main(string[] args) {
+			Console.CancelKeyPress += new ConsoleCancelEventHandler(InterruptReceived);
+
+			ImageRetriever imageRetriever = new ImageRetriever();
+			
+			udpClient.Connect("127.0.0.1", 50001);
+
+			while (true) {
+				byte[] image = imageRetriever.GetImage();
+				byte[] bytesToSend = Encoding.ASCII.GetBytes(ImageAnalyzer.AnalyzeImage(image));
+				udpClient.Send(bytesToSend, bytesToSend.Length);
+			}
+		}
+
+		private static void InterruptReceived(object sender, ConsoleCancelEventArgs e) {
+			Console.WriteLine("Interrup signal received, shutting down client.");
+			udpClient.Close();
+			Environment.Exit(0);
+		}
+	}
+}

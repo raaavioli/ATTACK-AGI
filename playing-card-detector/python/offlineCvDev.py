fb726e2ca3dde2aade4ae7db01022963c739041c
Oliver Eriksson <olieri@kth.se>
2021-10-13 11:00:43 +0200

    Change offlineCvDev to use 5 cards and add correctedImage with 5 cards.
    Also try improving template matching by applying some filters to the image, without any noticable success.

--- playing-card-detector/python/offlineCvDev.py
+++ playing-card-detector/python/offlineCvDev.py
@@ -6,28 +6,46 @@ import os
 
 file_path = os.path.dirname(__file__)
 
+CARD_AREAS = 5
 HEIGHT = 540
 WIDTH = 960
-RECT_HEIGHT = 90
-RECT_WIDTH = 120
+RECT_HEIGHT = HEIGHT / CARD_AREAS
+RECT_WIDTH = RECT_HEIGHT * 8.8 / 5.8 #dimension of playing card
 
 CARD_AREA_MIN = 5500
 CARD_AREA_MAX = 6000
 SUIT_AREA_MIN = 50
 SUIT_AREA_MAX = 500
 
-clubs = cv2.imread(file_path + "\..\sur40screenshots\clubs.png", cv2.IMREAD_GRAYSCALE)
+clubs = cv2.imread(file_path + "\..\sur40screenshots\club2.png", cv2.IMREAD_GRAYSCALE)
 
 def main():
-    print(file_path)
-    path = file_path + "\..\sur40screenshots\correctedImage.png"
+    path = file_path + "\..\sur40screenshots\correctedImage5.png"
     image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    print(analyzeImage(image))
+    sharpen_kernel = np.array(
+        [[-1,-1,-1,-1,-1], 
+        [-1,-1,-1,-1,-1], 
+        [-1,-1,25,-1,-1], 
+        [-1,-1,-1,-1,-1],
+        [-1,-1,-1,-1,-1]])
+    sharpen = cv2.filter2D(image, -1, sharpen_kernel)
+    
+    median = cv2.medianBlur(sharpen, 5)
+
+    size = (3, 3)
+    shape = cv2.MORPH_RECT
+    kernel = cv2.getStructuringElement(shape, size)
+    min_image = cv2.dilate(sharpen, kernel)
+
+    cv2.imshow("image", min_image)
+
+    #print(analyzeImage(median))
+    cv2.waitKey(0)
 
 # Takes a full-size image of the SUR40 screen and returns the string that represents the positioning of the cards.
 # The returned string is formatted as follows: "{player}:{position}:{suit}:{rank},{player}:{position}:{suit}:{rank},..."
 # Where player is 1 for the left-side player and 2 is for the right-side player,
-# position is a number 1-6 where 1 is the position at the top of the screen and 6 is the position at the bottom,
+# position is a number 0-CARD_AREAS where 0 is the position at the top of the screen and 6 is the position at the bottom,
 # suit is S for spades, C for clubs, D for diamonds and H for hearts,
 # rank is a number 1-13 where 1 is an ace and 13 is a king.
 def analyzeImage(image):
@@ -36,12 +54,9 @@ def analyzeImage(image):
     resultString = ""
     for i, subImage in enumerate(subImages):
         player = 1
-        if i > 5: player = 2
-        position = (i % 6) + 1
+        if i >= CARD_AREAS: player = 2
+        position = (i % CARD_AREAS)
         resultString += analyzeSubImage(subImage, player, position)
-
-    #cv2.imshow("image", image)
-    cv2.waitKey(0)
 
     return resultString
 
@@ -49,14 +64,14 @@ def analyzeImage(image):
 def splitImage(image):
     subImages = []
 
-    for i in range(12):
+    for i in range((2 * CARD_AREAS)):
         xOffset = 0
-        if i > 5: xOffset = WIDTH - RECT_WIDTH
-        yOffset = (i % 6) * RECT_HEIGHT
-        x1 = xOffset
-        x2 = xOffset + RECT_WIDTH
-        y1 = yOffset
-        y2 = yOffset + RECT_HEIGHT
+        if i >= CARD_AREAS: xOffset = WIDTH - RECT_WIDTH
+        yOffset = (i % CARD_AREAS) * RECT_HEIGHT
+        x1 = int(xOffset)
+        x2 = int(xOffset + RECT_WIDTH)
+        y1 = int(yOffset)
+        y2 = int(yOffset + RECT_HEIGHT)
         subImages.append(image[y1:y2, x1:x2])
 
     return subImages
@@ -80,8 +95,8 @@ def analyzeSubImage(subImage, player, position):
 
     img_contours = np.zeros(subImage.shape, dtype=np.uint8)
     cv2.drawContours(img_contours, contours, -1, 255, 1)
-    # cv2.imshow("thresh%d%d" % (player, position), thresh)
-    # cv2.imshow("img_contours%d%d" % (player, position), img_contours)
+    #cv2.imshow("thresh%d%d" % (player, position), thresh)
+    cv2.imshow("img_contours%d%d" % (player, position), img_contours)
 
     H, W = clubs.shape 
     result = cv2.matchTemplate(img_contours, clubs, cv2.TM_CCORR)
@@ -90,14 +105,14 @@ def analyzeSubImage(subImage, player, position):
     suit = "S"
     location = max_loc
     bottom_right = (location[0] + W, location[1] + H)
-    if (bottom_right[0] > (RECT_WIDTH / 2)):
+    if (bottom_right[0] > 30):
         suit = "C"
         cv2.rectangle(img_contours, location,bottom_right, 255, 1)
         cv2.imshow("img%d%d" % (player, position), img_contours)
 
     # Sobel testing
-    #sobel = cv2.Sobel(subImage,cv2.CV_64F,1,1,ksize=15)
-    #cv2.imshow("sobel%d%d" % (player, position), sobel)
+    # sobel = cv2.Sobel(subImage,cv2.CV_64F,1,1,ksize=15)
+    # cv2.imshow("sobel%d%d" % (player, position), sobel)
 
     rank = max(1, min(rank, 6))
     return "%d:%d:%s:%d," % (player, position, suit, rank)

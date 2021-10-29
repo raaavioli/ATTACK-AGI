13d4600260e3b3695a10307c5fa208cd16ce94bb
Filip Berendt <filipberendt11@gmail.com>
2021-10-29 15:07:14 +0200

    Made Buttons touchable, despite not having onclicks.
    It works with 10 card on screen, and it works well.

---
+++ ATTACK/Assets/UI/Menu/ButtonTouchScripts/QuitGameTouchScript.cs
@@ -0,0 +1,35 @@
+
+using System;
+using UnityEngine;
+using TouchScript.Gestures;
+
+public class QuitGameTouchScript : MonoBehaviour
+{
+    GameManager gameManager;
+    PressGesture press;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable() {
+        press = GetComponent<PressGesture>();
+        press.Pressed += pressHandler;
+    }
+
+    private void OnDisable() {
+        press.Pressed -= pressHandler;
+    }
+
+    private void pressHandler(object sender, EventArgs eventArgs) {
+        gameManager.ExitToMainMenu();
+        press.Cancel(true, true);
+    }
+}
